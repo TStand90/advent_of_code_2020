fcb3a0326734fda9136796aa29478ef54eee3af0
Tyler Standridge <tstand90@gmail.com>
2020-12-01 23:23:42 -0800

    Day 2 complete.

---
+++ AdventOfCode2020/Day2.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using static System.Int32;
+
+namespace AdventOfCode2020
+{
+    public static class Day2
+    {
+        private static readonly string FileName = Path.Combine(Environment.CurrentDirectory, @"inputs/day2.txt");
+        
+        public static int PartOne()
+        {
+            var validPasswordsCount = 0;
+            
+            var lines = File.ReadAllLines(FileName);
+
+            const string pattern = @"(\d+)-(\d+) (.): (.+)";
+
+            foreach (var line in lines)
+            {
+                foreach (System.Text.RegularExpressions.Match match in System.Text.RegularExpressions.Regex.Matches(line, pattern))
+                {
+                    var minimumValue = Parse(match.Groups[1].Value);
+                    var maximumValue = Parse(match.Groups[2].Value);
+                    var letter = Convert.ToChar(match.Groups[3].Value);
+                    var password = match.Groups[4].Value;
+
+                    var letterCount = password.Count(passwordLetter => passwordLetter == letter);
+
+                    if (letterCount >= minimumValue && letterCount <= maximumValue)
+                    {
+                        validPasswordsCount++;
+                    }
+                }
+            }
+
+            return validPasswordsCount;
+        }
+        
+        public static int PartTwo()
+        {
+            var validPasswordsCount = 0;
+            
+            var lines = File.ReadAllLines(FileName);
+
+            const string pattern = @"(\d+)-(\d+) (.): (.+)";
+
+            foreach (var line in lines)
+            {
+                foreach (System.Text.RegularExpressions.Match match in System.Text.RegularExpressions.Regex.Matches(line, pattern))
+                {
+                    var firstIndex = Parse(match.Groups[1].Value) - 1;
+                    var secondIndex = Parse(match.Groups[2].Value) - 1;
+                    var letter = Convert.ToChar(match.Groups[3].Value);
+                    var password = match.Groups[4].Value;
+
+                    if (password[firstIndex] == letter && password[secondIndex] != letter)
+                    {
+                        validPasswordsCount++;
+                    } else if (password[firstIndex] != letter && password[secondIndex] == letter)
+                    {
+                        validPasswordsCount++;
+                    }
+                }
+            }
+
+            return validPasswordsCount;
+        }
+    }
+}
--- AdventOfCode2020/Program.cs
+++ AdventOfCode2020/Program.cs
@@ -6,8 +6,11 @@ namespace AdventOfCode2020
     {
         private static void Main()
         {
-            Console.WriteLine(Day1.PartOne());
-            Console.WriteLine(Day1.PartTwo());
+            // Console.WriteLine(Day1.PartOne());
+            // Console.WriteLine(Day1.PartTwo());
+            
+            Console.WriteLine(Day2.PartOne());
+            Console.WriteLine(Day2.PartTwo());
         }
     }
 }
