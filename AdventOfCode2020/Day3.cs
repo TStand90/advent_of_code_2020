bc983c84c0a320968f20905a87ad377e248e652d
Tyler Standridge <tstand90@gmail.com>
2020-12-02 22:17:39 -0800

    Day 3 complete

---
+++ AdventOfCode2020/Day3.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace AdventOfCode2020
+{
+    public static class Day3
+    {
+        private static readonly string FileName = Path.Combine(Environment.CurrentDirectory, @"inputs/day3.txt");
+        
+        private static List<List<bool>> GetTrees()
+        {
+            var lines = File.ReadAllLines(FileName);
+
+            var trees = new List<List<bool>>();
+
+            foreach (var line in lines)
+            {
+                var treesRow = new List<bool>();
+                
+                for (var x = 0; x < lines[0].Length; x++)
+                {
+                    treesRow.Add(line[x] == '#');
+                }
+                
+                trees.Add(treesRow);
+            }
+
+            return trees;
+        }
+        
+        private static void Step(ref int positionX, ref int positionY, int dx, int dy, int width)
+        {
+            positionX += dx;
+            positionY += dy;
+            
+            if (positionX >= width)
+            {
+                positionX -= width;
+            }
+        }
+
+        private static int CalculateTreesOnSlope(IReadOnlyList<IReadOnlyList<bool>> trees, int dx, int dy)
+        {
+            var treesCount = 0;
+
+            var width = trees[0].Count;
+            var height = trees.Count;
+            
+            var currentPositionX = 0;
+            var currentPositionY = 0;
+            
+            while (currentPositionY < height - 1)
+            {
+                Step(ref currentPositionX, ref currentPositionY, dx, dy, width);
+                
+                if (trees[currentPositionY][currentPositionX])
+                {
+                    treesCount++;
+                }
+            }
+
+            return treesCount;
+        }
+        
+        public static int PartOne()
+        {
+            return CalculateTreesOnSlope(GetTrees(), 3, 1);
+        }
+
+        public static int PartTwo()
+        {
+            var trees = GetTrees();
+
+            var treesProduct = CalculateTreesOnSlope(trees, 1, 1);
+            treesProduct *= CalculateTreesOnSlope(trees, 3, 1);
+            treesProduct *= CalculateTreesOnSlope(trees, 5, 1);
+            treesProduct *= CalculateTreesOnSlope(trees, 7, 1);
+            treesProduct *= CalculateTreesOnSlope(trees, 1, 2);
+
+            return treesProduct;
+        }
+    }
+}
--- AdventOfCode2020/Program.cs
+++ AdventOfCode2020/Program.cs
@@ -9,8 +9,11 @@ namespace AdventOfCode2020
             // Console.WriteLine(Day1.PartOne());
             // Console.WriteLine(Day1.PartTwo());
             
-            Console.WriteLine(Day2.PartOne());
-            Console.WriteLine(Day2.PartTwo());
+            // Console.WriteLine(Day2.PartOne());
+            // Console.WriteLine(Day2.PartTwo());
+            
+            Console.WriteLine(Day3.PartOne());
+            Console.WriteLine(Day3.PartTwo());
         }
     }
 }
